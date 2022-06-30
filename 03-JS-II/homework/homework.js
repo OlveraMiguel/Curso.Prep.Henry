3b1d434dc707ef7106f50b4cf6c78aa89deabf23
OlveraMiguel <41132753+OlveraMiguel@users.noreply.github.com>
2022-06-30 10:27:01 -0500

    ok

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -7,11 +7,20 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  if(x > y){
-  return x;  
+  if ( x > y ) {
+    return x;
+  }
+  else if  (y > x) {
+    return y;
+  }
+  else {
+    return x;
+  }
+
+  
+
 }
-return y;
-}
+
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
@@ -129,6 +138,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0){
+    return "fizzbuzz";
+  }
+  else if (numero % 3 === 0){
+    return "fizz";
+  }
+  else if (numero % 5 === 0){
+    return "buzz";
+  }
+  else {
+    return numero;
+}
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -138,6 +159,25 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 < 0 || num2 < 0 || num3 < 0 ) {
+    return "Hay negativos";
+  } 
+  else if  (num1 > num2 && num1 > num3 && num1 > 0   ){
+    return "Número 1 es mayor y positivo";
+  }
+
+  else if (num1 === 0 || num2 === 0 || num3 ===0 ){
+    return "Error";
+  }
+
+  else if (num3 > num1 && num3 > num2){
+    return num3 + 1;
+  }
+  
+  else {
+    return false;
+  }
+
 }
 
 function esPrimo(numero) {
@@ -145,33 +185,59 @@ function esPrimo(numero) {
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
-  // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero <2) return false;
+  if (numero === 2) return false;
+  for (let i = 2; i < numero ; i++ ){
+    if(numero % i === 0){
+      return false;
+      break;
+    }
+  }   // Nota: Los números 0 y 1 NO son considerados números primos
+ return true;
 }
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  let r;
+ valor ? r = "Soy verdadero" : r = "Soy falso";
+ return r;
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  let arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
+  }
+  return arrayTablaDel6
+
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  let r ;
+  numero > 99 && numero < 1000 ? r= true : r = false;
+  return r ;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  let a = numero ;
+  let i = 0;
+  do{
+    a = a + 5;
+    i = i + 1;
+  } while (i<8);
+  return a ;
+
 }
 
 
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,22 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array [0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  fin = array.length;
+  return array [fin-1]
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +27,14 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var arr2 = [];
+  for (var i = 0 ; i < array.length ; i++){
+  arr2[i] = array [i] + 1;
+  }
+
+  
+
+
 }
 
 
